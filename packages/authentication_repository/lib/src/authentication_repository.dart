02dfcdd2ecc7688805e4aa50e49e84e076a3f152
saklani <50768838+saklani@users.noreply.github.com>
2023-10-29 17:21:18 +0530

    define auth function specs

---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,38 @@
+import "models/user.dart";
+import "enums/authentication_state.dart";
+
+/// The actual implementation of the [AuthenticationRepository]
+class AuthenticationRepository {
+
+  /// Returns the current authenticated user or null otherwise
+  User? get currentUser {
+    throw UnimplementedError();
+  }
+
+  /// A nullable Stream of type [User] which emits each time authentication state
+  /// is changed
+  Stream<User?> get user {
+    throw UnimplementedError();
+  }
+
+  /// Returns the current [AuthenticationState] of the user
+  Stream<AuthenticationState> get state {
+    throw UnimplementedError();
+  }
+
+  /// Creates an account for a user using an email and password, then, signs in
+  /// the user into the account.
+  Future<void> signUpWithEmailAndPassword(String email, String password) async {
+    throw UnimplementedError();
+  }
+
+  /// Signs in the user into their existing account.
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    throw UnimplementedError();
+  }
+
+  /// Signs out a user from their account.
+  Future<void> signOut() async {
+    throw UnimplementedError();
+  }
+}
---
+++ packages/authentication_repository/lib/src/enums/authentication_state.dart
@@ -0,0 +1,3 @@
+enum AuthenticationState {
+  unknown, unauthenticated, authenticated
+}
